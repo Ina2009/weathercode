7463922f72136ffef5e6a93516f4215c395c7441
Ina.croitoru <ina.croitoru@mail.ru>
2021-04-10 14:36:43 +0300

    applies DRY principle

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-import axios from "axios";
-//console.log(process.argv);
+import {
+  printCurrentWeather,
+  printCurrentWeatherFor7Days,
+} from "./weatherAPI.js";
 const city = process.argv[2];
-async function printCurrentWeather(cityName) {
-  const OPEN_WEATHER_MAP_API =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    cityName +
-    "&appid=6283374885c529df7c729538778fbfdd&units=metric&lang=ro";
 
-  try {
-    const response = await axios.get(OPEN_WEATHER_MAP_API);
-    let data = response.data;
-    console.log(`În ${data.name} se prognozează ${data.weather[0].description}. \n
-    Temperatura curentă este de ${data.main.temp}°C.`);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-printCurrentWeather(city);
+const coords = printCurrentWeather(city);
+printCurrentWeatherFor7Days(coords);
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,37 +1,33 @@
 import axios from "axios";
 import { OPEN_WEATHER_MAP_API_KEY } from "./credentials.js";
 
-async function getData(link) {
-  const response = await axios.get(link);
-  return response.data;
+async function getData(url) {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 export async function printCurrentWeather(cityName) {
   const OPEN_WEATHER_MAP_API =
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}` +
     `&appid=${OPEN_WEATHER_MAP_API_KEY}&units=metric&lang=ro`;
 
-  try {
-    let data = await getData(OPEN_WEATHER_MAP_API);
-    console.log(
-      `În ${data.name} se prognozează ${data.weather[0].description}.` +
-        `\nTemperatura curentă este de ${data.main.temp}°C.` +
-        `\nLong: ${data.coord.lon} Lat: ${data.coord.lat}`
-    );
-    printCurrentWeatherFor7Days(data.coord.lat, data.coord.lon);
-  } catch (error) {
-    console.log(error);
-  }
+  const data = await getData(OPEN_WEATHER_MAP_API);
+  console.log(
+    `În ${data.name} se prognozează ${data.weather[0].description}.` +
+      `\nTemperatura curentă este de ${data.main.temp}°C.` +
+      `\nLong: ${data.coord.lon} Lat: ${data.coord.lat}`
+  );
+  return data.coord;
 }
-
 export async function printCurrentWeatherFor7Days(lat, long) {
   const OPEN_WEATHER_MAP_API =
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}` +
     `&appid=${OPEN_WEATHER_MAP_API_KEY}&units=metric&lang=ro`;
 
-  try {
-    let data = await getData(OPEN_WEATHER_MAP_API);
-    console.log(data.daily.length);
-  } catch (error) {
-    console.log(error);
-  }
+  const data = await getData(OPEN_WEATHER_MAP_API);
+  console.log(data.daily.length);
 }
